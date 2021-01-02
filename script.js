f3b7e5ac48d68125eb33940d679ffa784b1bcef7
Michael <michaelleojacob@gmail.com>
2021-01-02 13:09:32 -0800

    took my console version of rockPaperScissors and gave it a UI. Refactored the code. Now there are more functions. Added click events and the ability to start the game via mouse click on one of the buttons. I still have some logic to write for the UI itself and fix a bug with the wonky p tags in round results. Looking forward to giving this project a better looking face as its a big ugly mess as far as colors and style.

---
+++ script.js
@@ -0,0 +1,150 @@
+let playerScore = 0;
+let computerScore = 0;
+const container = document.querySelector('.container');
+const rockbox = document.querySelector('.rockbox');
+const paperbox = document.querySelector('.paperbox');
+const scissorsbox = document.querySelector('.scissorsbox');
+const boxes = document.querySelectorAll('.playerbox');
+let rock = "rock";
+let paper = "paper";
+let scissors = "scissors";
+let roundWin = `You won the round!`;
+let roundLost = "You lost the round. ";
+let roundResults = document.querySelector('.roundResults');
+let scoreBoard;
+let pTagPlayerSelected = document.querySelector('#playerSelected');
+let pTagBotSelected = document.querySelector('#botselected');
+let pTagResult = document.querySelector('#resultptag');
+
+//!functions to increase score
+function increasePlayerScore(){
+    playerScore++;
+    scoreBoard = "Your score: " + playerScore + " Computer's score: " + computerScore + "\n \n";
+    return scoreBoard;
+}
+//!functions to increase score
+function increaseComputerScore(){
+    computerScore++;
+    scoreBoard = "Your score: " + playerScore + " Computer's score: " + computerScore + "\n \n";
+    return scoreBoard
+}
+
+
+//!gets random number (used for the computer to select r/p/s)
+function getRandomInt(max){
+    return Math.floor(Math.random() * Math.floor(max) + 1);
+}
+
+//!logic that handles the computer picking one: r/p/s
+function getComputerSelect(){
+
+    computerSelection = getRandomInt(3)
+    
+    if(computerSelection === 1){
+        computerSelection = rock;
+
+    }
+    else if(computerSelection === 2){
+        computerSelection = paper;
+    }
+    else if(computerSelection === 3){
+        computerSelection = scissors;
+    }
+    console.log("%cThe computer picked: " + computerSelection, "color:blue")
+    pTagBotSelected.innerText = `Bot selected: ${computerSelection}`;
+    return computerSelection;
+}
+
+//!code that allows boxes to be clicked on and starts a round.
+boxes.forEach(box => box.addEventListener('click', function (e){
+
+    if (e.target.innerText === "Rock"){
+        let playerSelection = rock;
+        pTagPlayerSelected.innerText = `You selected: ${playerSelection}`;
+        playRound(playerSelection, getComputerSelect());
+        
+    }
+    else if(e.target.innerText === "Paper"){
+        let playerSelection = paper;
+        pTagPlayerSelected.innerText = `You selected: ${playerSelection}`
+        playRound(playerSelection, getComputerSelect());
+    }
+    else if(e.target.innerText === "Scissors"){
+        let playerSelection = scissors;
+        pTagPlayerSelected.innerText = `You selected: ${playerSelection}`
+        playRound(playerSelection, getComputerSelect());
+    }
+    else{
+        return;
+    }
+}));
+
+
+//!round + game logic
+function playRound (playerSelection, computerSelection){   
+    //?tie
+    if (playerSelection === computerSelection){
+        console.log("%cThe round ended in a draw!", "color:orange");
+    }
+    
+    //?everything that is not a tie
+    if (playerSelection !== computerSelection){
+        if((playerSelection === rock) && (computerSelection === paper)){
+            console.log("%c" + roundLost, "color:red");
+            increaseComputerScore(); 
+        }
+        if((playerSelection === rock) && (computerSelection === scissors)){
+            console.log("%c" + roundWin, "color: green");
+            increasePlayerScore();
+        }
+        if((playerSelection === paper) && (computerSelection === rock)){
+            console.log("%c" + roundWin, "color: green");
+            increasePlayerScore();
+        }
+        if((playerSelection === paper) && (computerSelection === scissors)){
+            console.log("%c" + roundLost, "color:red");
+            increaseComputerScore();
+        }
+        if((playerSelection === scissors) && (computerSelection === rock)){
+            console.log("%c" + roundLost, "color:red");
+            increaseComputerScore();
+        }
+        if((playerSelection === scissors) && (computerSelection === paper)){
+            console.log("%c" + roundWin, "color: green");
+            increasePlayerScore();
+        }
+    }
+    pTagResult.innerText = scoreBoard
+    console.log(scoreBoard);
+}
+
+
+
+
+
+
+
+// function game(){
+
+//     console.log(`final score ${scoreBoard}`)
+
+//     if( playerScore > computerScore){
+//         console.log("%cYou won the match!", "color:green; font-size: 25px");
+//         console.log(`to replay, you can type "game()" again and press enter, or simply press the "up" arrow key on your keyboard and then press enter.`)
+//     }
+//     if( playerScore < computerScore){
+//         console.log("%cYou lost the match.", "color:red; font-size: 25px");
+//         console.log(`to replay, you can type "game()" again and press enter, or simply press the "up" arrow key on your keyboard and then press enter.`)
+
+//     }
+//     if( playerScore === computerScore){
+
+//         console.log("%cThe match ended in a draw.", "color:orange; font-size: 25px" )
+//         console.log(`to replay, you can type "game()" again and press enter, or simply press the "up" arrow key on your keyboard and then press enter.`)
+//     }
+
+
+//     playerScore = 0;
+//     computerScore = 0;
+
+// }
