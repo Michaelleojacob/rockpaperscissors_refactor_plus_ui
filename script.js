33e457be43bd710a7823b122cd96ad55320d7560
Michael <michaelleojacob@gmail.com>
2021-01-05 20:39:25 -0800

    game works, each round works. first to get 5 points works. It's a little ugly at the moment. But i'll clean up the css

--- script.js
+++ script.js
@@ -1,160 +1,112 @@
-let playerScore = 0;
-let computerScore = 0;
-const container = document.querySelector('.container');
-const rockbox = document.querySelector('.rockbox');
-const paperbox = document.querySelector('.paperbox');
-const scissorsbox = document.querySelector('.scissorsbox');
-const boxes = document.querySelectorAll('.playerbox');
-let rock = "rock";
-let paper = "paper";
-let scissors = "scissors";
-let roundWin = `You won the round!`;
-let roundLost = "You lost the round. ";
-let roundResults = document.querySelector('.roundResults');
-let scoreBoard;
-let pTagPlayerSelected = document.querySelector('#playerSelected');
-let pTagBotSelected = document.querySelector('#botselected');
-let pTagResult = document.querySelector('#resultptag');
-let gameResultDisplay = document.querySelector('#scoreBox');
-pTagPlayerSelected.textContent = `You picked:`;
-pTagBotSelected.textContent = `Bot picked:`;
-gameResultDisplay.textContent = `Score:`;
-const displayRound = document.querySelector("#round");
-const startbtn = document.querySelector('.button');
 const startScreen = document.querySelector('.startScreen');
-
+const container = document.querySelector('.container');
+const startbtn = document.querySelector('.button');
+let choices = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let compScore = 0;
+const optionBtn = document.querySelectorAll(`div.optionBtn`);
+let playerPoints = document.querySelector('#playerScore');
+let computerPoints = document.querySelector('#compScore');
+const roundResults = document.querySelector('#roundResults');
+const resetBtn = document.querySelector('#reset')
 
 //!start button from splash screen to game screen!!!!
 startbtn.addEventListener("click", function (e){
-    if(e.target.textContent === `start`){
         startScreen.classList.add('display-none');
         container.classList.add('fadeIn');
+        startGame();
+});
+
+function compPlay () {
+    let result = choices[Math.floor(Math.random() * choices.length)];
+    return result;
+}
+
+function playRound (playerSelection, computerSelection) {
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "Scissors") {
+            playerPoints.textContent = ++playerScore
+          roundResults.textContent =  "You win! Rock beats Scissors.";
+        }else if(computerSelection === "Paper"){
+            computerPoints.textContent = ++compScore
+          roundResults.textContent = "You lose! Paper beats rock.";
+        }else {
+            playerPoints.textContent = ++playerScore
+            computerPoints.textContent = ++compScore
+          roundResults.textContent = "Tie!"
+        }
+    }else if (playerSelection === 'paper') {
+      if (computerSelection === "Scissors") {
+            computerPoints.textContent = ++compScore
+          roundResults.textContent = "You lose! Scissors beats Paper!";
+        }else if (computerSelection === "Rock") {
+            playerPoints.textContent = ++playerScore
+          roundResults.textContent = "You win! Paper beats Rock!";
+        }else {
+            playerPoints.textContent = ++playerScore
+            computerPoints.textContent = ++compScore
+          roundResults.textContent = "Tie!";
+        }
+    }else if(playerSelection === "scissors") {
+      if (computerSelection === "Rock") {
+            computerPoints.textContent = ++compScore
+          roundResults.textContent = "You lose! Rock beats Scissors!";
+        }else if (computerSelection === "Paper") {
+            playerPoints.textContent = ++playerScore
+          roundResults.textContent = "You win! Scissors beats Paper!";
+        }else {
+            playerPoints.textContent = ++playerScore
+            computerPoints.textContent = ++compScore
+          roundResults.textContent = "Tie!"
+        }
     }
-    // return firstToFive();
-})
+   checkWinner();
+  }
+  resetGame();
 
-
-//!functions to increase score
-function increasePlayerScore(){
-    playerScore++;
-    scoreBoard = `Your score: ${playerScore} Bot's score: ${computerScore}`
-    return scoreBoard;
-}
-//!functions to increase score
-function increaseComputerScore(){
-    computerScore++;
-    scoreBoard = `Your score: ${playerScore} Bot's score: ${computerScore}`
-    return scoreBoard
-}
-//!function for draw, for consistency
-function draw(){
-    scoreBoard = `Your score: ${playerScore} Bot's score: ${computerScore}`
-    return scoreBoard;
-}
-
-
-//!gets random number (used for the computer to select r/p/s)
-function getRandomInt(max){
-    return Math.floor(Math.random() * Math.floor(max) + 1);
+  function checkWinner() {
+  if (compScore === 5 && playerScore === 5) {
+    roundResults.textContent = "The game is a Tie!";
+    roundResults.style.color ='blue';
+     optionBtn.forEach(button => {
+     button.removeEventListener('click', getPlayerChoice);
+   });
+  }else if (compScore === 5) {
+     roundResults.textContent = "You Lost the game to a computer!";
+     roundResults.style.color ='red';
+      optionBtn.forEach(button => {
+      button.removeEventListener('click', getPlayerChoice);
+    });
+  }else if (playerScore === 5) {
+    roundResults.textContent =  "You Win the game!!!!";
+    roundResults.style.color ='green';
+     optionBtn.forEach(button => {
+     button.removeEventListener('click', getPlayerChoice);
+   });
+  }
 }
 
-//!logic that handles the computer picking one: r/p/s
-function getComputerSelect(){
-
-    computerSelection = getRandomInt(3)
-    
-    if(computerSelection === 1){
-        computerSelection = rock;
-
-    }
-    else if(computerSelection === 2){
-        computerSelection = paper;
-    }
-    else if(computerSelection === 3){
-        computerSelection = scissors;
-    }
-    console.log("%cThe computer picked: " + computerSelection, "color:blue")
-    pTagBotSelected.textContent = `Bot selected: ${computerSelection}`;
-    return computerSelection;
-}
-
-//!code that allows boxes to be clicked on and starts a round.
-// function gameStart(boxes){
-    // boxes = document.querySelectorAll('.playerbox');
-    boxes.forEach(box => box.addEventListener('click', function (e){
+function resetGame(){
+    resetBtn.addEventListener('click', () =>{
+        container.classList.add('fadeIn');
+        playerScore = 0;
+        compScore = 0;
+        playerPoints.textContent = 0;
+        computerPoints.textContent = 0;
         
-        if (e.target.textContent === "Rock"){
-            let playerSelection = rock;
-            pTagPlayerSelected.textContent = `You selected: ${playerSelection}`;
-            playRound(playerSelection, getComputerSelect());
-            
-        }
-        else if(e.target.textContent === "Paper"){
-            let playerSelection = paper;
-            pTagPlayerSelected.textContent = `You selected: ${playerSelection}`
-            playRound(playerSelection, getComputerSelect());
-        }
-        else if(e.target.textContent === "Scissors"){
-            let playerSelection = scissors;
-            pTagPlayerSelected.textContent = `You selected: ${playerSelection}`
-            playRound(playerSelection, getComputerSelect());
-        }
-        else{
-            return;
-        }
-    }));
-// }
-    
-
-//!round + game logic
-function playRound (playerSelection, computerSelection){   
-    //?tie
-    if (playerSelection === computerSelection){
-        console.log("%cThe round ended in a draw!", "color:orange");
-        displayRound.textContent = `Draw`;
-        draw();
-    }
-    
-    //?everything that is not a tie
-    if (playerSelection !== computerSelection){
-        if((playerSelection === rock) && (computerSelection === paper)){
-            console.log("%c" + roundLost, "color:red");
-            displayRound.textContent = `Bot won this round`;
-            increaseComputerScore(); 
-        }
-        if((playerSelection === rock) && (computerSelection === scissors)){
-            console.log("%c" + roundWin, "color: green");
-            displayRound.textContent = `Player won this round`;
-            increasePlayerScore();
-        }
-        if((playerSelection === paper) && (computerSelection === rock)){
-            console.log("%c" + roundWin, "color: green");
-            displayRound.textContent = `Player won this round`;
-            increasePlayerScore();
-        }
-        if((playerSelection === paper) && (computerSelection === scissors)){
-            console.log("%c" + roundLost, "color:red");
-            displayRound.textContent = `Bot won this round`;
-            increaseComputerScore();
-        }
-        if((playerSelection === scissors) && (computerSelection === rock)){
-            console.log("%c" + roundLost, "color:red");
-            displayRound.textContent = `Bot won this round`;
-            increaseComputerScore();
-        }
-        if((playerSelection === scissors) && (computerSelection === paper)){
-            console.log("%c" + roundWin, "color: green");
-            displayRound.textContent = `Player won this round`;
-            increasePlayerScore();
-        }
-    }
-    console.log(scoreBoard);
-    gameResultDisplay.textContent = scoreBoard;
-
+        startGame();
+    })
 }
 
-// function firstToFive (playerSelection, computerSelection, playerScore, computerScore){
-//     gameStart();
-// }
+function startGame(){
+    optionBtn.forEach(button => {
+        button.addEventListener('click', getPlayerChoice);
+    });
+}
 
-
+function getPlayerChoice(e){
+    playerSelection = (e.target.id);
+    console.log(playerSelection);
+    playRound(playerSelection, compPlay())
+}
---
+++ scriptTwo.js
@@ -0,0 +1,159 @@
+let playerScore = 0;
+let computerScore = 0;
+const container = document.querySelector('.container');
+const rockbox = document.querySelector('.rockbox');
+const paperbox = document.querySelector('.paperbox');
+const scissorsbox = document.querySelector('.scissorsbox');
+const boxes = document.querySelectorAll('.playerbox');
+let rock = "rock";
+let paper = "paper";
+let scissors = "scissors";
+let roundWin = `You won the round!`;
+let roundLost = "You lost the round. ";
+let roundResults = document.querySelector('.roundResults');
+let scoreBoard;
+let pTagPlayerSelected = document.querySelector('#playerSelected');
+let pTagBotSelected = document.querySelector('#botselected');
+let pTagResult = document.querySelector('#resultptag');
+let gameResultDisplay = document.querySelector('#scoreBox');
+pTagPlayerSelected.textContent = `You picked:`;
+pTagBotSelected.textContent = `Bot picked:`;
+gameResultDisplay.textContent = `Score:`;
+const displayRound = document.querySelector("#round");
+const startbtn = document.querySelector('.button');
+const startScreen = document.querySelector('.startScreen');
+
+
+//!start button from splash screen to game screen!!!!
+startbtn.addEventListener("click", function (e){
+    if(e.target.textContent === `start`){
+        startScreen.classList.add('display-none');
+        container.classList.add('fadeIn');
+    }
+    // return firstToFive();
+})
+
+
+//!functions to increase score
+function increasePlayerScore(){
+    playerScore++;
+    scoreBoard = `Your score: ${playerScore} Bot's score: ${computerScore}`
+    return scoreBoard;
+}
+//!functions to increase score
+function increaseComputerScore(){
+    computerScore++;
+    scoreBoard = `Your score: ${playerScore} Bot's score: ${computerScore}`
+    return scoreBoard
+}
+//!function for draw, for consistency
+function draw(){
+    scoreBoard = `Your score: ${playerScore} Bot's score: ${computerScore}`
+    return scoreBoard;
+}
+
+
+//!gets random number (used for the computer to select r/p/s)
+function getRandomInt(max){
+    return Math.floor(Math.random() * Math.floor(max) + 1);
+}
+
+//!logic that handles the computer picking one: r/p/s
+function getComputerSelect(){
+
+    computerSelection = getRandomInt(3)
+    
+    if(computerSelection === 1){
+        computerSelection = rock;
+
+    }
+    else if(computerSelection === 2){
+        computerSelection = paper;
+    }
+    else if(computerSelection === 3){
+        computerSelection = scissors;
+    }
+    console.log("%cThe computer picked: " + computerSelection, "color:blue")
+    pTagBotSelected.textContent = `Bot selected: ${computerSelection}`;
+    return computerSelection;
+}
+
+//!code that allows boxes to be clicked on and starts a round.
+// function gameStart(boxes){
+    // boxes = document.querySelectorAll('.playerbox');
+    boxes.forEach(box => box.addEventListener('click', function (e){
+        
+        if (e.target.textContent === "Rock"){
+            let playerSelection = rock;
+            pTagPlayerSelected.textContent = `You selected: ${playerSelection}`;
+            playRound(playerSelection, getComputerSelect());
+            
+        }
+        else if(e.target.textContent === "Paper"){
+            let playerSelection = paper;
+            pTagPlayerSelected.textContent = `You selected: ${playerSelection}`
+            playRound(playerSelection, getComputerSelect());
+        }
+        else if(e.target.textContent === "Scissors"){
+            let playerSelection = scissors;
+            pTagPlayerSelected.textContent = `You selected: ${playerSelection}`
+            playRound(playerSelection, getComputerSelect());
+        }
+        else{
+            return;
+        }
+    }));
+// }
+    
+
+//!round + game logic
+function playRound (playerSelection, computerSelection){   
+    //?tie
+    if (playerSelection === computerSelection){
+        console.log("%cThe round ended in a draw!", "color:orange");
+        displayRound.textContent = `Draw`;
+        draw();
+    }
+    
+    //?everything that is not a tie
+    if (playerSelection !== computerSelection){
+        if((playerSelection === rock) && (computerSelection === paper)){
+            console.log("%c" + roundLost, "color:red");
+            displayRound.textContent = `Bot won this round`;
+            increaseComputerScore(); 
+        }
+        if((playerSelection === rock) && (computerSelection === scissors)){
+            console.log("%c" + roundWin, "color: green");
+            displayRound.textContent = `Player won this round`;
+            increasePlayerScore();
+        }
+        if((playerSelection === paper) && (computerSelection === rock)){
+            console.log("%c" + roundWin, "color: green");
+            displayRound.textContent = `Player won this round`;
+            increasePlayerScore();
+        }
+        if((playerSelection === paper) && (computerSelection === scissors)){
+            console.log("%c" + roundLost, "color:red");
+            displayRound.textContent = `Bot won this round`;
+            increaseComputerScore();
+        }
+        if((playerSelection === scissors) && (computerSelection === rock)){
+            console.log("%c" + roundLost, "color:red");
+            displayRound.textContent = `Bot won this round`;
+            increaseComputerScore();
+        }
+        if((playerSelection === scissors) && (computerSelection === paper)){
+            console.log("%c" + roundWin, "color: green");
+            displayRound.textContent = `Player won this round`;
+            increasePlayerScore();
+        }
+    }
+    console.log(scoreBoard);
+    gameResultDisplay.textContent = scoreBoard;
+
+}
+
+// function firstToFive (playerSelection, computerSelection, playerScore, computerScore){
+//     gameStart();
+// }
+
